27859ac776bc96d1a2e69629f3f281b152b94339
Jose Manuel Quintas <failop@Joses-MacBook-Pro.local>
2021-02-09 11:39:59 -0500

    modifications

--- Front_end/src/Component/Landing.js
+++ Front_end/src/Component/Landing.js
@@ -1,4 +1,7 @@
 import React from "react";
+import "./../css/Landing.css"
+import pict from "./../Image/PICTURE.png"
+import search_p from "./../Image/BUTTON_search.png"
 
 
 class landing extends React.Component
@@ -13,9 +16,41 @@ class landing extends React.Component
           return (
                 <div className="App">
                   <header className="App-header">
-                    <p>
-                      Landing page.
-                    </p>
+                    <h1> Stock Predictor App </h1>
+                    <div className="land_box">   
+                      <div className="land_box2">
+                        <h2 className="land_text"> About Us </h2>
+                        <p className="land_text">
+                          This is Senior project, search yourr stock, be prepare, we will tell you your future, but you take the risk
+                        </p>
+                      </div>
+                      <div className="land_hole2"> </div>
+                      <div className="land_box2">
+                        <img src={[pict]} align='center'  />
+                      </div>
+                    </div>
+
+                    <div className="land_hole"> &nbsp; </div>
+
+                    <div className="land_box">   
+                      <div className="land_box2">
+                        <h2 className="land_text"> Search </h2>
+                        <img src={[search_p]} className="land_Icon" align='center'  />
+                        <p className="land_text"> --------- </p>
+                        <form className="Nav_Base">
+                          <input className="Nav_Base" type="text"  placeholder = "Seach ...."  name="name"  />
+                        </form>
+                      </div>
+                      <div className="land_hole2"> </div>
+                      <div className="land_box2">
+                        <h2 className="land_text"> User </h2>
+                        <div className="land_box3" style = { { display: 'center' }} > REGISTER </div>
+                        <div className="land_box3"  display='center' > SIGN UP </div>
+                      </div>
+                    </div>
+
+
+                    <div className="land_hole"> &nbsp; </div>
                   </header>
                 </div>
               );
--- Front_end/src/Nav.js
+++ Front_end/src/Nav.js
@@ -1,52 +1,106 @@
 import './css/Nav.css';
-// import React, { Component }  from "react";
+import React, { Component }  from "react";
 // import Image from 'react-image-resizer';
 import logo from "./Image/BUTTON_search.png"
 import profile from "./Image/Profile.png"
 import pictureX from "./Image/PICTURE.png"
 
-
-const Nav = () => {
+class Nav extends React.Component
+{
   // console.log(currentUser);
 
-  var handleClick_landing = () =>
-  {
-    console.log("Opertion");
-    window.location.href = '/Landing';
-  }
+  constructor(props) 
+        {
+                super(props);
+        }
 
-  var handleClick_Search = () =>
-  {
-    console.log("Opertion2");
-    window.location.href = '/search';
-  }
+        render()
+        {
 
-  var handleClick_User = () =>
-  {
-    console.log("Opertion3");
-    window.location.href = '/user';
-  }
+          var handleClick_landing = () =>
+          {
+            console.log("Opertion");
+            window.location.href = '/Landing';
+          }
 
-    return (
-       <div className="CoreX2">
-     {/* <Image source={logo} 
-     style={{ alignSelf: 'center', height: 150, width: 150,
-     borderWidth: 1, borderRadius: 75 }} /> */}
-        <img src={pictureX} className="Nav_Icon2" onClick={()=> handleClick_landing()}/>
-        
-        <div className="Nav_Base">
-            
-        </div>
-        <form className="Nav_Base">
-          <input className="Nav_Base" type="text"  placeholder = "Seach ...."  name="name"  />
-        </form>
-        <img src={logo} className="Nav_Icon" align='center' onClick={()=> handleClick_Search()} />
-        
-        <div className="hole3"> </div>
-        <img src={[profile]} className="Nav_Icon" align='right' onClick={()=> handleClick_User()}  />
+          var handleClick_Search = () =>
+          {
+            console.log("Opertion2");
+            window.location.href = '/search';
+          }
 
-      </div>
-    );
+          var handleClick_User = () =>
+          {
+            console.log("Opertion3");
+            window.location.href = '/user';
+          }
+
+            return (
+              <div className="CoreX2">
+            {/* <Image source={logo} 
+            style={{ alignSelf: 'center', height: 150, width: 150,
+            borderWidth: 1, borderRadius: 75 }} /> */}
+                <img src={pictureX} className="Nav_Icon2" onClick={()=> handleClick_landing()}/>
+                
+                <div className="Nav_Base">
+                    
+                </div>
+                <form className="Nav_Base">
+                  <input className="Nav_Base" type="text"  placeholder = "Seach ...."  name="name"  />
+                </form>
+                <img src={logo} className="Nav_Icon" align='center' onClick={()=> handleClick_Search()} />
+                
+                <div className="hole3"> </div>
+                <img src={[profile]} className="Nav_Icon" align='right' onClick={()=> handleClick_User()}  />
+
+              </div>
+            );
+        }
 }
 
 export default Nav;
+
+// const Nav = (props) => {
+//   // console.log(currentUser);
+
+//   var handleClick_landing = () =>
+//   {
+//     console.log("Opertion");
+//     window.location.href = '/Landing';
+//   }
+
+//   var handleClick_Search = () =>
+//   {
+//     console.log("Opertion2");
+//     window.location.href = '/search';
+//   }
+
+//   var handleClick_User = () =>
+//   {
+//     console.log("Opertion3");
+//     window.location.href = '/user';
+//   }
+
+//     return (
+//        <div className="CoreX2">
+//      {/* <Image source={logo} 
+//      style={{ alignSelf: 'center', height: 150, width: 150,
+//      borderWidth: 1, borderRadius: 75 }} /> */}
+//         <img src={pictureX} className="Nav_Icon2" onClick={()=> handleClick_landing()}/>
+        
+//         <div className="Nav_Base">
+            
+//         </div>
+//         <form className="Nav_Base">
+//           <input className="Nav_Base" type="text"  placeholder = "Seach ...."  name="name"  />
+//         </form>
+//         <img src={logo} className="Nav_Icon" align='center' onClick={()=> handleClick_Search()} />
+        
+//         <div className="hole3"> </div>
+//         <img src={[profile]} className="Nav_Icon" align='right' onClick={()=> handleClick_User()}  />
+
+//       </div>
+//     );
+// }
+
+// export default Nav;
