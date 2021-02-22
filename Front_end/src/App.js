86cb0a2b6cc2baea650837391a44b78cc1f4c96b
JMo95 <33079476+JMo95@users.noreply.github.com>
2021-02-21 22:49:50 -0500

    New Nav Bar
    Created a new nav bar and integrated it with the components.

--- Front_end/src/App.js
+++ Front_end/src/App.js
@@ -2,7 +2,7 @@ import './css/App.css';
 import React from "react";
 // import {render} from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from './Nav';
+import BootstrapNavbar from './GlobalNav';
 //import {Router, Route} from "react-router";
 
 import landing from "./Component/Landing";
@@ -11,6 +11,9 @@ import userX from "./Component/User";
 import regist_P from "./Component/Register";
 import log_in from "./Component/Log_in";
 //import { AuthProvider } from './components/Auth';
+
+//Bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 //function App() {
@@ -39,7 +42,7 @@ class App extends React.Component
         {
           return (
             <div className="App"> 
-            <Nav search_F = {this.handleSearch}  />
+            <BootstrapNavbar search_F = {this.handleSearch}  />
             {this.handle_pass_Search}
               <Router>
                 {/* <div className="App_Nav"> */}
