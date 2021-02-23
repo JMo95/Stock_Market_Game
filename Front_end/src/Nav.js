a53f213ac8e757b652effd1fa2d4deb7ebb8c660
JMo95 <joshuamorin@ufl.edu>
2021-02-23 00:19:04 -0500

    Updated Name on Nav

--- Front_end/src/Nav.js
+++ Front_end/src/Nav.js
@@ -87,7 +87,7 @@ class BootstrapNavbar extends React.Component{
                     <div className="col-md-12">
                         <Router>
                             <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                                <Navbar.Brand href="/">React Bootstrap Navbar</Navbar.Brand>
+                                <Navbar.Brand href="/">Stock Price Predictor</Navbar.Brand>
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="mr-auto">
