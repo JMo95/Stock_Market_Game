9a6b932235d6b1d2ca58c11de2a440d277b9fce3
Joshua S Morin <33079476+JMo95@users.noreply.github.com>
2021-04-10 19:23:05 -0400

    Updated home page

--- Front_end/src/Component/Landing.js
+++ Front_end/src/Component/Landing.js
@@ -1,95 +1,57 @@
 import React from "react";
 import "./../css/Landing.css"
-import pict from "./../Image/PICTURE.png"
+import pict from "./../Image/Stock.jfif"
 import search_p from "./../Image/BUTTON_search.png"
+import {Button, Container, Row, Col, Table, Form, Card} from 'react-bootstrap'
 
 
 class landing extends React.Component
 {
-        constructor(props) 
-        {
-          super(props);
-        }
+  constructor(props) 
+  {
+    super(props);
+  }
 
+  render()
+  {
+      return(
+          <Container>
+          <h1>CEN 4914 Beginner Stock Trading Resource</h1>
+          <Row>
+              <Col>
+              <p className="mt-5"></p>
+              <h3>About Us</h3>
+              <p className="text-md-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque iaculis arcu a dui auctor pellentesque. Fusce nec lectus scelerisque magna ultricies finibus at eleifend arcu. Aliquam lacinia blandit erat, ac eleifend neque dapibus ut. Quisque varius molestie nisl rhoncus tempus. Maecenas elementum vestibulum tellus. Cras cursus varius venenatis. Aenean sit amet felis turpis. Nulla eu porta libero, in laoreet felis. Ut sagittis nisi ut ipsum tincidunt porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies viverra diam nec pulvinar. Vestibulum interdum dolor sit amet congue imperdiet. Proin bibendum ante nec sem bibendum, ac ultricies sem tincidunt. Maecenas molestie id lectus pellentesque fermentum. In in viverra libero. Maecenas at maximus tellus. Ut ac interdum elit. Donec euismod vitae dui at ullamcorper. Sed tincidunt enim quis erat laoreet faucibus. Etiam pretium euismod massa non tempor. Etiam blandit justo in dignissim commodo.</p>
+              <p className="mt-5"></p>
+              <h3>How To Use</h3>
+              <p className="text-md-left">
+                  <ul>
+                  <li>Navbar: </li>
+                  <li>Search Page: </li>
+                  <li>Trading Page: </li>
+                  <li>User: </li>
+                  </ul>
+              </p>
+              </Col>
 
-        handleClick_Search2 = () =>
-          {
-            console.log("OpertionX");
-            window.location.href = '/search';
-          }
+              <Col>
+              <p className="mt-5"></p>
+              <Card className="bg-dark text-white">
+              <Card.Img src={[pict]} height={360} alt="Card image" />
+              <Card.ImgOverlay>
+                  <Card.Title></Card.Title>
+              </Card.ImgOverlay>
+              </Card>
+              <p className="mt-5"></p>
+              <h3>User Information</h3>
+                  <p>Nulla eu porta libero, in laoreet felis. Ut sagittis nisi ut ipsum tincidunt porttitor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ultricies viverra diam nec pulvinar. Vestibulum interdum dolor sit amet congue imperdiet. Proin bibendum ante nec sem bibendum, ac ultricies sem tincidunt. Maecenas molestie id lectus pellentesque fermentum. In in viverra libero. Maecenas at maximus tellus. Ut ac interdum elit. Donec euismod vitae dui at ullamcorper. Sed tincidunt enim quis erat laoreet faucibus. Etiam pretium euismod massa non tempor. Etiam blandit justo in dignissim commodo.</p>
+              </Col>
 
-        handleClick_Sign_in = () =>
-          {
-            console.log("Opertion_Sign");
-            window.location.href = '/Log_in';
-          }
-
-        handleClick_Regsiter = () =>
-          {
-            console.log("Opertion_Reg");
-            window.location.href = '/Register';
-          }
-
-          handleSubmit_search(event)
-         {
-          event.preventDefault();
-            console.log("Submit happen");
-            alert('A value was submitted: ');
-            window.location.href = '/search';
-        }
-
-        render()
-        {
-          // var handleClick_Search = () =>
-          // {
-          //   console.log("Opertion2");
-          //   window.location.href = '/search';
-          // }
-
+          </Row>
           
-
-          return (
-                <div className="App">
-                  <header className="App-header">
-                    <h1> Stock Predictor App </h1>
-                    <div className="land_box">   
-                      <div className="land_box2">
-                        <h2 className="land_text"> About Us </h2>
-                        <p className="land_text">
-                          This is Senior project, search yourr stock, be prepare, we will tell you your future, but you take the risk
-                        </p>
-                      </div>
-                      <div className="land_hole2"> </div>
-                      <div className="land_box2">
-                        <img src={[pict]} align='center' alt="future imagee for stock price" />
-                      </div>
-                    </div>
-
-                    <div className="land_hole"> &nbsp; </div>
-
-                    <div className="land_box">   
-                      <div className="land_box2">
-                        <h2 className="land_text"> Search </h2>
-                        <img src={[search_p]} className="land_Icon" align='center' onClick={()=> this.handleClick_Search2()} alt="standard search button"  />
-                        <p className="land_text"> --------- </p>
-                        <form className="Nav_Base" onSubmit={this.handleSubmit_search}>
-                          <input className="Nav_Base" type="text"  placeholder = "Seach ...."  name="name"  />
-                        </form>
-                      </div>
-                      <div className="land_hole2"> </div>
-                      <div className="land_box2">
-                        <h2 className="land_text"> User </h2>
-                        <div className="land_box3" style = { { display: 'center' }} onClick={()=> this.handleClick_Regsiter()} > REGISTER </div>
-                        <div className="land_box3"  display='center' onClick={()=> this.handleClick_Sign_in()} > SIGN UP </div>
-                      </div>
-                    </div>
-
-
-                    <div className="land_hole"> &nbsp; </div>
-                  </header>
-                </div>
-              );
-        }
+          </Container>
+      );
+  }
 }
 
 export default landing;
