f11a91495bdceafe38bb284a2326c64664cac80c
Jose Manuel Quintas <44034603+Josequintas96@users.noreply.github.com>
2021-04-17 14:00:56 -0400

    Add stick is functional as it is the User Authtication

--- Front_end/src/Component/Trading.js
+++ Front_end/src/Component/Trading.js
@@ -1,23 +1,129 @@
-import React from "react";
+import React  , { useState, useEffect, Fragment }  from "react";
 import {Button, Container, Row, Col, Table, Form} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from "axios";
 import token from './token';
 
 
-class Trading extends React.Component
-{
-    constructor(props) 
-    {
-          super(props);
-    }
 
-    state = {   
-        token: {}
+const Trading = () => {
 
-    }
+    const [email, setEmail] = useState('')
+    const [money, setMoney] = useState('')
+    const [bearerX, setbearerX] = useState('')
+    const [user, setUser] = useState('')
+    const [username, setUsername] = useState('')
+    const [stock_name, setStock_name] = useState('')
+    const [quantity, setQuantity] = useState('')
+    const [price, setPrice] = useState('')
+    const [loading, setLoading] = useState(true);
+    const [errors, setErrors] = useState(false);
+  
+    useEffect(() => {
+      if (localStorage.getItem('token') == null) {
+        window.location.replace('http://localhost:3000/');
+      } else {
+        setLoading(false);
+      }
 
-    async componentDidMount() {
+      var myHeaders = new Headers();
+        var bearer = "bearer "
+        // console.log("This is my bearer: ", bearer)
+        bearer += localStorage.getItem('token')
+        // console.log("This is my bearer: ", bearer)
+        myHeaders.append("Authorization", bearer);
+        setbearerX(bearer)
+  
+        var requestOptions = {
+          method: 'GET',
+          headers: myHeaders,
+          redirect: 'follow'
+        };
+  
+        var tomato = {
+          "money": 0,
+          "user__email": "",
+          "user__username": "",
+        };
+  
+        
+        fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getuser/", requestOptions)
+        .then(response => response.json())
+        .then(function(result){
+          console.log("This is a result: ", result[0])
+          setUsername(result[0].user__username)
+          setEmail(result[0].user__email)
+          setMoney(result[0].money)
+  
+         } )
+        .catch(function(error){
+            console.log('error', error) 
+            console.log("Tomato")
+            localStorage.clear()
+             setErrors(true)
+           });
+
+    }, []);
+  
+  
+    // const handleUser = e => {
+  
+    //     var myHeaders = new Headers();
+    //     var bearer = "bearer "
+    //     // console.log("This is my bearer: ", bearer)
+    //     bearer += localStorage.getItem('token')
+    //     // console.log("This is my bearer: ", bearer)
+    //     myHeaders.append("Authorization", bearer);
+  
+    //     var requestOptions = {
+    //       method: 'GET',
+    //       headers: myHeaders,
+    //       redirect: 'follow'
+    //     };
+  
+    //     var tomato = {
+    //       "money": 0,
+    //       "user__email": "",
+    //       "user__username": "",
+    //     };
+        
+  
+        
+    //     fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getuser/", requestOptions)
+    //     .then(response => response.json())
+    //     .then(function(result){
+    //       console.log("This is a result: ", result[0])
+    //       // console.log("This is a map user: ", result[0].user__username)
+    //       // console.log("This is a map email: ", result[0].user__email)
+    //       // console.log("This is a username: ", result[0].getElementBy('user_username'))
+    //       // console.log("This is a result: ", result[0].user_username)
+    //       // tomato["user__username"] = result[0].user__username
+    //       // tomato.money = result[0].money
+    //       // tomato.user__email = result[0].user__email
+    //       setUsername(result[0].user__username)
+    //       setEmail(result[0].user__email)
+    //       setMoney(result[0].money)
+  
+    //      } )
+    //     .catch(error => console.log('error', error));
+  
+    //   };
+
+
+// class Trading extends React.Component
+// {
+//     constructor(props) 
+//     {
+//           super(props);
+//     }
+
+//     state = {   
+//         token: {}
+
+//     }
+
+    // async componentDidMount() {
+    const componentDidMount = e => {
         // Simple POST request with a JSON body using axios
         //const article = { title: 'React POST Request Example' };
         const dataForm = new FormData();
@@ -29,16 +135,17 @@ class Trading extends React.Component
         }
 
 
-        token.value = await axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/login/', dataForm, 
-        );
+        // token.value = await axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/login/', dataForm, );
 
+        token.value =  axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/login/', dataForm, );
         
         console.log(token.value.data.token);
 
         const userdataForm = new FormData();
         userdataForm.append('username', "regtest");
         
-        var res = await axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getstocks/', userdataForm);
+        // var res = await axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getstocks/', userdataForm);
+        var res =  axios.post('https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getstocks/', userdataForm);
 
         console.log(res);
         
@@ -47,101 +154,141 @@ class Trading extends React.Component
         //     .then(response => this.setState({token : response.data}));
     }
 
-    render()
+    function onSucess(e)
     {
+        console.log("Sucess happen on the user: ", username)
+        console.log("This is stock name: ", stock_name)
+        console.log("This is quantity: ", quantity)
+        console.log("This is money: ", money)
+
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", bearerX);
+
+        var formdata = new FormData();
+        formdata.append("quantity", quantity);
+        formdata.append("stock", stock_name); //GME
+        formdata.append("user", username);
+
+        var requestOptions = 
+        {
+            method: 'POST',
+            headers: myHeaders,
+            body: formdata,
+            redirect: 'follow'
+        };
+
+        fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/addstock/", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+    }
+
+    // render()
+    // {
 
         return(
             
             <div>
-                <Container>
-                    <Row>
-                        <p>User Test</p>
-                        <p> </p>
-                        <Col><h4>Balance: $420.69</h4></Col>
-                        <Col></Col>
-                        <Col></Col>
-                    </Row>
-                    <p className="mt-5"></p>
-                    <Row>
-                        <Col xs={6}>
-                            <h2>Your Stock List</h2>
-                            <p className="mt-5"></p>
-                            <Table striped hover>
-                            <thread>
-                                <tr>
-                                    <th>Symbol</th>
-                                    <th>Name</th>
-                                    <th>Price</th>
-                                    <th>Quantity</th>
-                                </tr>
-                            <tbody>
-                                <tr>
-                                    <th>GME</th>
-                                    <th>Gamestop</th>
-                                    <th>$349.22</th>
-                                    <th>233</th>
-                                </tr>
-                                <tr>
-                                    <th>AMC</th>
-                                    <th>AMC Entertainment</th>
-                                    <th>$49.22</th>
-                                    <th>2333</th>
-                                </tr>
-                                <tr>
-                                    <th>RKT</th>
-                                    <th>RKT Mortgage</th>
-                                    <th>$33.22</th>
-                                    <th>213</th>
-                                </tr>
-                                <tr>
-                                    <th>TLRY</th>
-                                    <th>Tilray</th>
-                                    <th>$19.85</th>
-                                    <th>42</th>
-                                </tr>
-                            </tbody>
-                            </thread>
-                            </Table>
-                        </Col>
-                        <Col xs={6}>
-                            <h2> Buy/Sell</h2>
-                            <p className="mt-4"></p>
-                            <Form>
-                                <Form.Row>
-                                    <Col xs={6}>
+            {errors ==true?
+                <div> You had gotten Logout, your account has expired</div>
+                :
+                <div> <Container>
+                <Row>
+                    <p>User Test</p>
+                    <p> </p>
+                    <Col><h4>Balance: $420.69</h4></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+                <p className="mt-5"></p>
+                <Row>
+                    <Col xs={6}>
+                        <h2>Your Stock List</h2>
+                        <p className="mt-5"></p>
+                        <Table striped hover>
+                        <thread>
+                            <tr>
+                                <th>Symbol</th>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Quantity</th>
+                            </tr>
+                        <tbody>
+                            <tr>
+                                <th>GME</th>
+                                <th>Gamestop</th>
+                                <th>$349.22</th>
+                                <th>233</th>
+                            </tr>
+                            <tr>
+                                <th>AMC</th>
+                                <th>AMC Entertainment</th>
+                                <th>$49.22</th>
+                                <th>2333</th>
+                            </tr>
+                            <tr>
+                                <th>RKT</th>
+                                <th>RKT Mortgage</th>
+                                <th>$33.22</th>
+                                <th>213</th>
+                            </tr>
+                            <tr>
+                                <th>TLRY</th>
+                                <th>Tilray</th>
+                                <th>$19.85</th>
+                                <th>42</th>
+                            </tr>
+                        </tbody>
+                        </thread>
+                        </Table>
+                    </Col>
+                    <Col xs={6}>
+                        <h2> Buy/Sell</h2>
+                        <p className="mt-4"></p>
+                        <Form>
+                            <Form.Row>
+                                <Col xs={6}>
 
-                                    <h6>Stock Name</h6>
-                                    <Form.Control placeholder="Enter stock name..." />
-                                    <p className="mt-2"></p>
-                                    
-                                    </Col>
-                                    <Col>
-                                    <h6>Quantity</h6>
-                                    <Form.Control placeholder="Qty..." />
-                                    </Col>
-                                    <Col>
-                                    <h6>Price</h6>
-                                    <Form.Control placeholder="$4383.33" />
-                                    </Col>
-                                </Form.Row>
-                                </Form>
-                                <Row>
-
-                                <Col>
-                                <Button variant="success" size="lg" block>Buy</Button>
+                                <h6>Stock Name</h6>
+                                {/* <input></input> */}
+                                <Form.Control placeholder="Enter stock name..." onChange={(e) => setStock_name(e.target.value) } />
+                                {/* ; 
+                                    console.log("This is the text value ", e.target.value )
+                                */}
+                                <p className="mt-2"></p>
+                                
                                 </Col>
                                 <Col>
-                                <Button variant="danger" size="lg" block>Sell</Button>  
+                                <h6>Quantity</h6>
+                                <Form.Control placeholder="Qty..." onChange={(e) => setQuantity(e.target.value) }/>
                                 </Col>
-                                </Row>
-                        </Col>
-                    </Row>
-                </Container>
+                                <Col>
+                                <h6>Price</h6>
+                                <Form.Control placeholder="$4383.33" onChange={(e) => setMoney(e.target.value) }/>
+                                </Col>
+                            </Form.Row>
+                            <Row>
+                                <Col>
+                                    <Button variant="success" onClick={(e) => onSucess(e)} size="lg" block >Buy</Button>
+                                    {/*   */}
+
+                                </Col>
+                                <Col>
+                                    <Button variant="danger" size="lg" block>Sell</Button>  
+                                </Col>
+                            </Row>
+                        </Form>
+                            
+                    </Col>
+                </Row>
+            </Container></div>
+                }
             </div>
             
             
         );
-    }
+    // }
 
 }
 export default Trading;
--- Front_end/src/Component/User.js
+++ Front_end/src/Component/User.js
@@ -3,15 +3,13 @@ import "./../css/User.css"
 import search_p from "./../Image/BUTTON_search.png"
 
 
-
-
 const User = () => {
 
   const [email, setEmail] = useState('')
   const [money, setMoney] = useState('')
   const [user, setUser] = useState('')
   const [username, setUsername] = useState('')
-
+  const [errors, setErrors] = useState(false);
 
 
   const [loading, setLoading] = useState(true);
@@ -22,57 +20,93 @@ const User = () => {
     } else {
       setLoading(false);
     }
+    var myHeaders = new Headers();
+    var bearer = "bearer "
+    // console.log("This is my bearer: ", bearer)
+    bearer += localStorage.getItem('token')
+    // console.log("This is my bearer: ", bearer)
+    myHeaders.append("Authorization", bearer);
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+
+    var tomato = {
+      "money": 0,
+      "user__email": "",
+      "user__username": "",
+    };
+    
+
+    
+    fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getuser/", requestOptions)
+    .then(response => response.json())
+    .then(function(result){
+      // console.log("This is a result: ", result[0])
+      // console.log("This is a map user: ", result[0].user__username)
+      // console.log("This is a map email: ", result[0].user__email)
+      // console.log("This is a username: ", result[0].getElementBy('user_username'))
+      // console.log("This is a result: ", result[0].user_username)
+      // tomato["user__username"] = result[0].user__username
+      // tomato.money = result[0].money
+      // tomato.user__email = result[0].user__email
+      setUsername(result[0].user__username)
+      setEmail(result[0].user__email)
+      setMoney(result[0].money)
+
+     } )
+    .catch(function(error){
+       console.log('error', error) 
+       console.log("Tomato")
+       localStorage.clear()
+        setErrors(true)
+      });
+// localStorage.clear(), setErrors(true)
   }, []);
 
 
   const handleUser = e => {
 
-      var myHeaders = new Headers();
-      var bearer = "bearer "
-      // console.log("This is my bearer: ", bearer)
-      bearer += localStorage.getItem('token')
-      console.log("This is my bearer: ", bearer)
-      myHeaders.append("Authorization", bearer);
+      // var myHeaders = new Headers();
+      // var bearer = "bearer "
+      // // console.log("This is my bearer: ", bearer)
+      // bearer += localStorage.getItem('token')
+      // // console.log("This is my bearer: ", bearer)
+      // myHeaders.append("Authorization", bearer);
 
-      var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-      };
+      // var requestOptions = {
+      //   method: 'GET',
+      //   headers: myHeaders,
+      //   redirect: 'follow'
+      // };
 
-      var tomato = {
-        "money": 0,
-        "user__email": "",
-        "user__username": "",
-      };
+      // var tomato = {
+      //   "money": 0,
+      //   "user__email": "",
+      //   "user__username": "",
+      // };
       
 
       
-      fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getuser/", requestOptions)
-      .then(response => response.json())
-      .then(function(result){
-        // console.log("This is a result: ", result[0])
-        // console.log("This is a map user: ", result[0].user__username)
-        // console.log("This is a map email: ", result[0].user__email)
-        // console.log("This is a username: ", result[0].getElementBy('user_username'))
-        // console.log("This is a result: ", result[0].user_username)
-        // tomato["user__username"] = result[0].user__username
-        // tomato.money = result[0].money
-        // tomato.user__email = result[0].user__email
-        setUsername(result[0].user__username)
-        setEmail(result[0].user__email)
-        setMoney(result[0].money)
+      // fetch("https://stock-pipeli-users-fje2brrt8yy.herokuapp.com/getuser/", requestOptions)
+      // .then(response => response.json())
+      // .then(function(result){
+      //   // console.log("This is a result: ", result[0])
+      //   // console.log("This is a map user: ", result[0].user__username)
+      //   // console.log("This is a map email: ", result[0].user__email)
+      //   // console.log("This is a username: ", result[0].getElementBy('user_username'))
+      //   // console.log("This is a result: ", result[0].user_username)
+      //   // tomato["user__username"] = result[0].user__username
+      //   // tomato.money = result[0].money
+      //   // tomato.user__email = result[0].user__email
+      //   setUsername(result[0].user__username)
+      //   setEmail(result[0].user__email)
+      //   setMoney(result[0].money)
 
-       } )
-      .catch(error => console.log('error', error));
-
-      console.log("This is a username Omega on =>  ", username)
-
-      // console.log("This is a result Omega on =>  ", tomato)
-      // console.log("This is a user_username on =>  ", tomato.user__username)
-      // console.log("This is a user__email on =>  ", tomato.user__email)
-      // console.log("This is a money on =>  ", tomato.money)
-
+      //  } )
+      // .catch(error => console.log('error', error));
 
     };
 
@@ -95,49 +129,57 @@ const User = () => {
 
   return (
 
-                <div className="App">
-                  <div>BELLOW LAY YOUR TOKEN</div>
+    <div className="App">
+          {/* <div>BELLOW LAY YOUR TOKEN</div>
           <div> { localStorage.getItem('token') } </div>
           <div> This is the username: { username } </div>
           {
             handleUser()
             // console.log("Print stuff: ",value)
-          }
-                  <header className="App-header">
-                  <div className="user_box">   
-                      <div className="user_box2">
-                        <h2 className="user_text"> { username }  </h2>
-                        <div className="user_box4">
-                            <h3> Profile </h3>
-                            <p className="user_text2"> Name: {username}</p>
-                            <p className="user_text2"> Email: {email}</p>
-                            <p className="user_text2"> Deposit money: {money}</p>
-                        </div>
+          } */}
+      <header className="App-header">
+        <div className="user_box">   
+        {errors ==true?
+        <div> You had gotten Logout, your account has expired</div>
+        :
+        <div>
+<div className="user_box2">
+            <h2 className="user_text"> { username }  </h2>
+            <div className="user_box4">
+              <h3> Profile </h3>
+              <p className="user_text2"> Name: {username}</p>
+              <p className="user_text2"> Email: {email}</p>
+              <p className="user_text2"> Deposit money: {money}</p>
+            </div>
 
-                        <div className="user_box4">
-                            <h3> Pin sotcks </h3>
-                            <p className="user_text2"> Name: </p>
-                            <p className="user_text2"> Usename: </p>
-                            <p className="user_text2"> Deposit money: </p>
-                        </div>
+            <div className="user_box4">
+              <h3> Pin sotcks </h3>
+              <p className="user_text2"> Name: </p>
+              <p className="user_text2"> Usename: </p>
+              <p className="user_text2"> Deposit money: </p>
+            </div>
+          </div>
 
-                      </div>
+          <div className="user_hole2"> </div>
 
-                      <div className="user_hole2"> </div>
+          <div className="user_box2">
+            <h2 className="user_text"> Search </h2>
+              <img src={[search_p]} className="land_Icon" align='center' onClick={()=> handleClick_Search2()} alt="standard search button" />
+              <p className="user_text"> --------- </p>
+              <form className="user_Base" onSubmit={handleSubmit_search}>
+                <input className="user_Base" type="text"  placeholder = "Seach ...."  name="name"  />
+              </form>
+          </div>
 
-                      <div className="user_box2">
-                        <h2 className="user_text"> Search </h2>
-                        <img src={[search_p]} className="land_Icon" align='center' onClick={()=> handleClick_Search2()} alt="standard search button" />
-                        <p className="user_text"> --------- </p>
-                        <form className="user_Base" onSubmit={handleSubmit_search}>
-                          <input className="user_Base" type="text"  placeholder = "Seach ...."  name="name"  />
-                        </form>
-                      </div>
-                    </div>
 
-                  </header>
+        </div>
+        }
+        
+          
+        </div>
 
-                </div>
+      </header>
+    </div>
               );
         // }
 }
